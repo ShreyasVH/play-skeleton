b56dd1f69bd1fbdb8f6226845f372b93828aa606
Shreyas Hande <shreyas@everwell.org>
2022-01-09 00:23:02 +0530

    adding docker support

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: target/universal/stage/bin/play-skeleton -jvm-debug "8021"
--- start.sh
+++ start.sh
@@ -1,7 +1,1 @@
-#!/usr/bin/env bash
-
-set -o allexport
-source .env;
-set +o allexport
-
-activator -jvm-debug "8000" -Dhttps.port="10000" "run 9000";
+sbt -jvm-debug "8000" -Dhttps.port="10000" "run 9000";
