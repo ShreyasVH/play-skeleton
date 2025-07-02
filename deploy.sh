416fc2603435905bafdd02ff887183e056e71e65
ShreyasVH <shreyas.deve.1@gmail.com>
2025-07-02 08:35:52 +0530

    temp changes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
+source $HOME/.zshrc
+cd $HOME/workspace/myProjects/play/play-skeleton
+
 if ! lsof -i :$PORT > /dev/null; then
+    pwd
     echo "Starting"
+    env
     ./dist/bin/$REPO_NAME -Dhttp.port=$PORT > server.log 2>&1 &
 fi
