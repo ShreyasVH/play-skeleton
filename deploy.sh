2bc69209927a93b84d30a37ac503b3aa4cc55b8c
ShreyasVH <shreyas.deve.1@gmail.com>
2025-07-03 16:46:20 +0530

    updates

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,6 @@
-source $HOME/.zshrc
-cd $HOME/workspace/myProjects/java/play/play-skeleton
-# source .envrc
-echo $PATH
-ls
-
+cd $(realpath $(dirname deploy.sh))
 
 if ! lsof -i :$PORT > /dev/null; then
-    pwd
     echo "Starting"
-    env
     ./dist/bin/$REPO_NAME -Dhttp.port=$PORT > server.log 2>&1 &
 fi
