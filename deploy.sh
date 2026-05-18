0da2a47f70b8507e66397b73ab0d14f2e73616c8
ShreyasVH <shreyas.deve.1@gmail.com>
2026-05-18 19:22:42 +0530

    removed logs

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,4 @@ cd $(realpath $(dirname deploy.sh))
 if ! lsof -i :$PORT > /dev/null; then
     echo "Starting"
     ./dist/bin/$REPO_NAME -Dhttp.port=$PORT > server.log 2>&1 &
-    echo "$JAVA_HOME" >> server.log
-    echo "$PATH" >> server.log
-    java --version >> server.log
 fi
